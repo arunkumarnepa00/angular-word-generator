ce02cbefda288f18c85954c80e37e7d062e76ba3
arunkumarnepa00 <arunkumarnepa00@gmail.com>
2023-01-29 14:41:35 +0530

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import arrayWords from 'src/utils/words';
+import countryArray from 'src/utils/countries';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words='';
+  limit:number=0;
+  handleSlideChange(newLimit:number){
+    this.limit=newLimit;
+  }
+  generate(){
+    this.words=arrayWords.slice(0,this.limit).join(" ");
+  }
+  countries='';
+  // countryLimit=0;
+  // handleCountryCount(newCount:number){
+  //   this.countryLimit=newCount;
+  // }
+  generateCountries=()=>{
+    this.countries=countryArray.slice(0,this.limit).join(",");
+  }
+
 }
